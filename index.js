494f2a2a086a9af5fc7e881e6a0577d676c734db
tskkks <130366264+tskkks@users.noreply.github.com>
2024-10-04 15:08:12 +0000

    criado rotas home e sobre. iniciado menu

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const PORT = 3301;
 
 app.get("/", function (req, res) {
   // res.sendfile();
-  res.senf(__dirname);
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  // res.sendfile();
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
